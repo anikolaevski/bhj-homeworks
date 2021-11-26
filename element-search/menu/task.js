7e00e2bda3de4e1e3a5607dd60146cfbf3fa500a
Alexei Nikolaevski <anikolaevski@gmail.com>
2021-11-26 14:15:33 +0300

    task 2.1 done

---
+++ element-search/menu/task.js
@@ -0,0 +1,34 @@
+const menu_sub = document.querySelectorAll('.menu_sub');
+const active_items = [];
+
+for (let k = 0; k < menu_sub.length; k++) {
+  // присваиваем ID элементу меню
+  if (!menu_sub[k].id) {
+    menu_sub[k].setAttribute('id', `menu_item${k}`);
+  }
+  
+  // получаем ссылку на пункт верхнего меню
+  const menu_item = menu_sub[k].closest('ul .menu__item');
+  // подписываемся на показ меню
+  menu_item.onclick = function() {
+    // выключение активных меню
+    active_items.forEach((o) => {
+      const el = document.getElementById(o);
+      if (el) {
+        el.classList.remove('menu_active');
+      }
+    });
+    // активация выбранного меню
+    menu_sub[k].classList.add('menu_active');
+    // сохранение активного меню
+    active_items.splice(0, active_items.length);
+    active_items.push(menu_sub[k].id);
+  }
+
+  // Получаем дочернюю ссылку пункта меню
+  const link = menu_item.querySelector('a');
+  // запрещаем клик на ссылке
+  link.onclick = function(evt) {
+    evt.preventDefault();
+  } 
+}
