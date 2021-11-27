c2ded94bdc59f49da1c38b5624de65dbc47c7b9c
anikolaevski@gmail.com <anikolaevski@gmail.com>
2021-11-27 14:16:31 +0300

    popup fixed

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,18 +1,27 @@
 const modal_main = document.getElementById('modal_main');
 const show_success = document.querySelector('.show-success');
 const modal_success = document.getElementById('modal_success');
+const modal_close_times = document.querySelectorAll('.modal__close_times');
 
-if(modal_main) {
-	modal_main.classList.add('modal_active');
-	modal_main.querySelector('.modal__close').onclick = function() {
-		console.log('Main popup closed!!!');
-		modal_main.classList.remove('modal_active');
+for ( let k = 0; k < modal_close_times.length; k++ ) {
+	modal_close_times[k].onclick = function() {
+		const popup = modal_close_times[k].closest('.modal');
+		if (popup) {
+			popup.classList.remove('modal_active');
+		}
 	}
 }
 
-if (show_success) {
+if(modal_main) {
+	modal_main.classList.add('modal_active');
+}
+
+if (show_success && modal_success) {
 	show_success.onclick = function (evt) {
 		console.log('show_success!!!');
 		modal_success.classList.add('modal_active');
+		if(modal_main) {
+			modal_main.classList.remove('modal_active');
+		}
 	}
 }
