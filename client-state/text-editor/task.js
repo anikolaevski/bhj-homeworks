5b4c09942012fca72a419500548edb2b791b68d4
Alexey Nikolaevskiy <anikolaevski@gmail.com>
2021-12-11 21:01:20 +0300

    client-state/text-editor done

---
+++ client-state/text-editor/task.js
@@ -0,0 +1,29 @@
+const storageName = 'Netology-editor';
+const editor = document.getElementById('editor');
+editor.insertAdjacentHTML('afterend','<button id="clear__button">Очистить</button>')
+
+editor.addEventListener('input', function(evt) {
+    evt.preventDefault();
+    saveContent(this.value);
+});
+
+document.getElementById('clear__button').addEventListener('click', function(evt) {
+    evt.preventDefault();
+    editor.value = '';
+    saveContent('');
+});
+
+function loadContent() {
+    try {
+        const loaded = localStorage.getItem(storageName);
+        editor.value = loaded;        
+    } catch(err) {
+        alert (err);
+    }
+}
+
+function saveContent(value) {
+    localStorage.setItem(storageName, value);
+}
+
+loadContent();
